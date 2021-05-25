03849e91f2dae181f185757371d3f2118bdbdb39
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-05-25 19:59:35 -0300

    login

--- src/usecases/Auth/auth.ts
+++ src/usecases/Auth/auth.ts
@@ -8,32 +8,32 @@ class Auth {
   async login(request: Request, response: Response) {
     const {login, pass}  = request.body
     const senha =  Encrypt('sha1', pass)
-    // try {
-    //   await knex('usuario').select('idusuario','login',"nome",'idtipousuario','ativo')
-    //     .where({login, senha})
-    //   .then((data: any) => {
-    //     if(data[0].idusuario){
-    //       const token = jwt.sign({id: data.idusuario}, process.env.JWT_SECRET || '', {
-    //         expiresIn: '24h'
-    //       })
-    //       response.json({...data[0], auth: true, token});
-    //     }else{
-    //       response.status(401).json({message: 'Login inválido'})
-    //     }
-    //   })
-    // .catch(e => response.status(400).json({err: e}))
-    // } catch (error) {
-    //   response.status(400).json({error})
-    // }
-    knex.raw(`SELECT * from usuario where login = '${login}' and senha = '${senha}'`)
-    .then( (message) => {
-      // Success / boot rest of app
-      response.json(message.rows)
-    }).catch( (err) => {
-      // Failure / timeout
-      console.log(err)
-      throw err
-    })
+    try {
+      await knex('usuario').select('idusuario','login',"nome",'idtipousuario','ativo')
+        .where({login, senha})
+      .then((data: any) => {
+        if(data[0].idusuario){
+          const token = jwt.sign({id: data.idusuario}, process.env.JWT_SECRET || '', {
+            expiresIn: '24h'
+          })
+          response.json({...data[0], auth: true, token});
+        }else{
+          response.status(401).json({message: 'Login inválido'})
+        }
+      })
+    .catch(e => response.status(400).json({err: e}))
+    } catch (error) {
+      response.status(400).json({error})
+    }
+    // knex.raw(`SELECT * from usuario where login = '${login}' and senha = '${senha}'`)
+    // .then( (message) => {
+    //   // Success / boot rest of app
+    //   response.json(message.rows)
+    // }).catch( (err) => {
+    //   // Failure / timeout
+    //   console.log(err)
+    //   throw err
+    // })
   }
 }
 
