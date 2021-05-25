5c262c35802efc602145bbade850d62590bb3a20
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-05-25 18:37:18 -0300

    Login

--- src/usecases/Auth/auth.ts
+++ src/usecases/Auth/auth.ts
@@ -6,21 +6,26 @@ import {Encrypt} from '../../utils'
 
 class Auth {
   async login(request: Request, response: Response) {
-		const {login, pass}  = request.body
-    const senha =  Encrypt('sha1', pass)
-		await knex('usuario').select('*')
-			.where({login, senha}).debug(true)
-    .then((data: any) => {
-      console.log(data)
-      if(data[0].idusuario){
-        const token = jwt.sign({id: data.idusuario}, process.env.JWT_SECRET || '', {
-          expiresIn: '24h'
-        })
-        return response.json({...data[0], auth: true, token});
-      }
-      return response.status(401).json({message: 'Login inválido'})
-    })
+    try {
+      const {login, pass}  = request.body
+      const senha =  Encrypt('sha1', pass)
+      await knex('usuario').select('*')
+        .where({login, senha}).debug(true)
+      .then((data: any) => {
+        console.log(data)
+        if(data[0].idusuario){
+          const token = jwt.sign({id: data.idusuario}, process.env.JWT_SECRET || '', {
+            expiresIn: '24h'
+          })
+          return response.json({...data[0], auth: true, token});
+        }
+        return response.status(401).json({message: 'Login inválido'})
+      })
     .catch(e => response.status(400).json({err: e}))
+    } catch (error) {
+      response.status(400).json({error})
+    }
+		
   }
 }
 
