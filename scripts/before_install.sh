bf914244aa2fe7f24e151f4c47a666e404da984c
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-05-27 11:09:37 -0300

    Add.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf /home/ubuntu/app/*
 sudo rm -rf /home/ubuntu/app/node_modules
-sudo rm -rf /home/ubuntu/app/.git
+sudo rm -rf /home/ubuntu/app/scripts
+sudo rm -rf /home/ubuntu/app/src
 sudo rm -rf /home/ubuntu/app/.gitignore
