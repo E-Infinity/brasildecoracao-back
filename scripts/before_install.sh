c989499f7336de8aba4d03438b0c37655c22259a
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-05-27 10:05:01 -0300

    Deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,2 @@
 #!bin/bash
 
-#Criar diretório caso não exista
-DIR="/home/ubuntu/app"
-if [ -d "$DIR" ]; then
-    echo "${DIR} exists"
-else
-    echo "Creating ${DIR} directory"
-    mkdir ${DIR}
-fi
