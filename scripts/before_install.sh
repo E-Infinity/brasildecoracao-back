659eed63a00854dd0d6aef64c89520ec8587daa0
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-05-28 09:46:36 -0300

    DeploySH

--- scripts/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf /home/ubuntu/app/*
-sudo rm -rf /home/ubuntu/app/node_modules
-sudo rm -rf /home/ubuntu/app/scripts
-sudo rm -rf /home/ubuntu/app/src
-sudo rm -rf /home/ubuntu/app/.gitignore
