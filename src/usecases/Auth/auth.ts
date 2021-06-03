b8a86f6d9e8dda8a90c9d03150f530fcb53f92f7
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-06-02 23:30:21 -0300

    Login

--- src/usecases/Auth/auth.ts
+++ src/usecases/Auth/auth.ts
@@ -12,7 +12,7 @@ class Auth {
       await knex('usuario as u').select('u.idusuario','u.login',"u.nome","u.email",'u.idtipousuario', 't.descricao as tipousuario_desc','u.ativo')
         .leftJoin('tipousuario as t','u.idtipousuario','t.idtipousuario')
         .where({login, senha})
-        .where('ativo', true).debug(true)
+        .where('u.ativo', true).debug(true)
       .then((data: any) => {
         if(data[0].idusuario){
           const token = jwt.sign({id: data.idusuario}, process.env.JWT_SECRET || '', {
