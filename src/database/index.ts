25a527a62f34729cd21abda90556f18a9f6a3da2
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-06-24 12:01:17 -0300

    Up

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,8 +8,7 @@ const connection = knex({
     host : process.env.DB_HOST ,
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
-    database : process.env.DB_NAME,
-    ssl: true
+    database : process.env.DB_NAME
   }
 })
 
