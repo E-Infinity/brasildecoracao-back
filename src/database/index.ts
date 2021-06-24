1d99ebaa0b2e677ea578f3712efcda17c67fed12
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-06-24 11:33:09 -0300

    Up

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,40 +1,15 @@
 import knex from 'knex'
-require("dotenv").config();
-
-console.log(process.env.HOST, process.env.USER, process.env.PASSWORD, process.env.DATABASE, process.env.DBPORT)
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const connection = knex({
-    client: 'pg',
-    connection: 'postgres://pguoxpkunbojve:5308d927b4357d7464eda6d89b5f76e2383cb9ccb6adfdf08cb24df62f694c87@ec2-54-197-100-79.compute-1.amazonaws.com:5432/d6kgri96p21uvn',
-    searchPath: ['knex', 'public'],
-    // connection: {
-    //     host: 'ec2-54-197-100-79.compute-1.amazonaws.com',
-    //     user : 'pguoxpkunbojve',
-    //     password : '5308d927b4357d7464eda6d89b5f76e2383cb9ccb6adfdf08cb24df62f694c87',
-    //     database : 'd6kgri96p21uvn',
-    //     port: 5432
-    // }, 
-    pool: {
-      afterCreate: function (conn: any, done: any) {
-        // in this example we use pg driver's connection API
-        conn.query('SELECT 1; ', function (err: any) {
-          if (err) {
-            // first query failed, return error and don't try to make next query
-            done(err, conn);
-          } else {
-            // do the second query...
-            conn.query('SELECT 1;', function (err:any) {
-              // if err is not falsy, connection is discarded from pool
-              // if connection aquire was triggered by a query the error is passed to query promise
-              done(err, conn);
-            });
-          }
-        });
-        console.log('Passou')
-      }
-    }
+  client: 'pg',
+  connection:  {
+    host : process.env.DB_HOST ,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
+  }
 })
-
-
 
 export default connection
