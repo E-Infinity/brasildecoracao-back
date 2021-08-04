c499cbf01b9a0dbbc7be1fd012753173835045bc
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-08-04 19:29:33 -0300

    Add id on rotes

--- src/usecases/Product/colorfibra.ts
+++ src/usecases/Product/colorfibra.ts
@@ -4,7 +4,7 @@ import knex from "../../database";
 class ColorFibra {
   async list(request: Request, response: Response){
     const {idcorfibra} = request.params
-    const sql = knex('corfibra').select(knex.raw('row_number as id'),'*')
+    const sql = knex('corfibra').select(knex.raw('row_number() OVER () as id'),'*')
     if(idcorfibra){
       sql.where({idcorfibra})
     }
