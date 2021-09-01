100b95e228d91dbc113229d9a4bcec75087fe35e
Giovanni Figueiredo <giovanni@fastcore.com.br>
2021-09-01 11:38:58 -0300

    Update Route list inventory

--- src/usecases/Product/inventory.ts
+++ src/usecases/Product/inventory.ts
@@ -3,10 +3,28 @@ import knex from "../../database";
 
 class Inventory {
   async list(request: Request, response: Response){
-    const {idproduto} = request.params
-    const sql = knex('estoque').select('*')
-    if(idproduto){
-      sql.where({idproduto})
+    const {idprodutograde} = request.params
+    const sql = knex('estoque as e').select('e.*',knex.raw(`
+      f.descricao as filial_desc,
+      f.cidade as  filial_cidade,
+      f.uf as filial_uf,
+      pg.idproduto,
+      pg.idcoraluminio,
+      pg.idcorfibra,
+      pg.idtrama,
+      p.descricao as produto,
+      ca.descricao as coraluminio,
+      t.descricao as trama,
+      cf.descricao as corfibra
+    `))
+    .leftJoin('filial as f', 'f.idfilial','e.idfilial' )
+    .leftJoin('produtograde as pg','pg.idprodutograde','e.idprodutograde')
+    .leftJoin('produto as p','p.idproduto','pg.idproduto')
+    .leftJoin('coraluminio as ca','ca.idcoraluminio','pg.idcoraluminio')
+    .leftJoin('trama as t','t.idtrama','pg.idtrama')
+    .leftJoin('corfibra as cf','cf.idcorfibra','pg.idcorfibra')
+    if(idprodutograde){
+      sql.where('e.idprodutograde',idprodutograde)
     }
     sql.then(data => response.json(data))
   }
