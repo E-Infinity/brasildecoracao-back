72a72a809a3442d2a523ecf669db7f59f8a4ecf8
Giovanni Vinicius Souza de Figueiredo <giovannivinicius1997@gmail.com>
2021-12-01 20:12:20 -0300

    Remove Cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import routes from './routes'
 require('dotenv').config()
-import cors from 'cors'
+//import cors from 'cors'
 var timeout = require('connect-timeout')
 
 const app = express()
-app.use(cors())
+//app.use(cors())
 app.use(express.json({limit: '4096mb'}))
 app.use(timeout('6000s'))
 
