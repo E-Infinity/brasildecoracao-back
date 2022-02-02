7f2af709d426883520d7e9d7bd7b00352bd72d80
Giovanni Figueiredo <giovanni@fastcore.com.br>
2022-02-02 17:58:06 -0300

    Fix

--- src/usecases/Debts/debts.ts
+++ src/usecases/Debts/debts.ts
@@ -36,11 +36,10 @@ class Debts {
           periodo ? where += `AND datavencimento::date BETWEEN '${periodo[0]}' AND '${periodo[1]}' ` : ''
         }
         const parcelas = await knex('contaspagarparcela').select('*')
-          .whereRaw(where).debug(true).orderBy('datavencimento')
+          .whereRaw(where).orderBy('datavencimento')
           
         contas.push({
           ...d,
-          datavencimento: parcelas[0].datavencimento,
           parcelas
         })
       }
