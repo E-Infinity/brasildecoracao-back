99fb81f0880cca3bd4b94f4abfb551e0ea483d3b
Giovanni Figueiredo <giovannivinicius1997@gmail.com>
2022-03-03 21:44:50 -0300

    Tipo contas pagar

--- src/usecases/Debts/index.ts
+++ src/usecases/Debts/index.ts
@@ -1,7 +1,9 @@
 const TypeDocument = require('./typeDocument')
+const TypeDebts = require('./TypeDebts')
 const Debts = require('./debts')
 
 module.exports = {
   TypeDocument,
+  TypeDebts,
   Debts,
 }
