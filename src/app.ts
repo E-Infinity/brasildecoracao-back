396c95f96fb458a890e3b0d5600f58642d86242e
Giovanni Figueiredo <giovannivinicius1997@gmail.com>
2022-03-15 21:45:31 -0300

    Update

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,18 @@ import cors from 'cors'
 var timeout = require('connect-timeout')
 
 const app = express()
-app.use(cors())
 app.use(express.json({limit: '4096mb'}))
 app.use(timeout('60000s'))
+app.use(cors())
 
 app.use(routes)
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('Server running on port: 3333')
