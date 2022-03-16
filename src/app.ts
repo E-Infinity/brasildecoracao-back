e047e2cf0e81ff3739f47adc2e639e1e1386e614
Giovanni Figueiredo <giovannivinicius1997@gmail.com>
2022-03-15 21:46:32 -0300

    Update

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,9 @@ app.use(cors())
 
 app.use(routes)
 
-app.use(function(req, res, next) {
+app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
