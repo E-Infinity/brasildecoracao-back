6385708757dcd48d24cf8ca9eb8fa5d357980359
Giovanni Figueiredo <giovannivinicius1997@gmail.com>
2022-03-15 21:58:13 -0300

    Update

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,11 @@ const app = express()
 app.use(express.json({limit: '4096mb'}))
 app.use(timeout('60000s'))
 app.use(cors({
-  origin: 'https://sistema.brasildecoracao.com'
+  origin: ['https://sistema.brasildecoracao.com'],
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }))
 
 app.use(routes)
-
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('Server running on port: 3333')
